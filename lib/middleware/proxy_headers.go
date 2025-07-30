70cce7872f6265bf10c985ccd93b44c185a86d3f
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-07-30 12:57:09 -0700

    added some error handling

--- lib/middleware/proxy_headers.go
+++ lib/middleware/proxy_headers.go
@@ -3,14 +3,36 @@ package middleware
 import (
 	"net"
 	"net/http"
+
+	"github.com/reiver/go-http500"
 )
 
 // ProxyHeaders sets the proxy headers before proxying the request
 func ProxyHeaders(next http.Handler) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	if nil == next {
+		return nil
+	}
 
-		rip, rport, _ := net.SplitHostPort(r.RemoteAddr)
-		host, _, _ := net.SplitHostPort(r.Host)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if nil == w {
+			return
+		}
+		if nil == r {
+			http500.InternalServerError(w, r)
+			return
+		}
+
+		rip, rport, err := net.SplitHostPort(r.RemoteAddr)
+		if nil != err {
+			http500.InternalServerError(w, r)
+			return
+		}
+
+		host, _, err := net.SplitHostPort(r.Host)
+		if nil != err {
+			http500.InternalServerError(w, r)
+			return
+		}
 
 		r.Header.Set("Host", host)
 		r.Header.Set("X-Forwarded-Proto", "https")
