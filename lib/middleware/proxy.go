0fab6c0af232da07342d7e61e78fb1e7df7a4e71
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-07-30 13:06:41 -0700

    added some error handling

--- lib/middleware/proxy.go
+++ lib/middleware/proxy.go
@@ -6,6 +6,8 @@ import (
 	"net/http/httputil"
 	"sync"
 
+	"github.com/reiver/go-http500"
+
 	"github.com/reiver/space-portal/cfg"
 )
 
@@ -13,10 +15,27 @@ var proxies = new(sync.Map)
 
 // Proxy proxies the request to space-base
 func Proxy(next http.Handler) http.Handler {
+	if nil == next {
+		return nil
+	}
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		host, _, _ := net.SplitHostPort(r.Host)
-		proxy, ok := proxies.Load(host)
-		if !ok {
+		if nil == w {
+			return
+		}
+		if nil == r {
+			http500.InternalServerError(w, r)
+			return
+		}
+
+		host, _, err := net.SplitHostPort(r.Host)
+		if nil != err {
+			http500.InternalServerError(w, r)
+			return
+		}
+
+		proxy, found := proxies.Load(host)
+		if !found {
 			proxy = httputil.NewSingleHostReverseProxy(cfg.SpaceBaseAddress()) // use .yml file instead of env variable to configure multiple space-base addresses
 			proxies.Store(host, proxy)
 		}
